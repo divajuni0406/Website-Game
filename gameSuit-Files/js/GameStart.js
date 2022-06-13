275afcbb91a6287d7504600b7b7faf4cc6cd2253
Diva Juni Artha <divajuni0406@gmail.com>
2022-06-13 11:33:00 +0800

    move every section elements in javascript to the new files js to make it to be module javascript, and delete onclick in html and make onclick event in javascript

---
+++ gameSuit-Files/js/GameStart.js
@@ -0,0 +1,25 @@
+// Object Oriented Programming
+class GameStart {
+  constructor() {
+    this.comWin = "COM WIN";
+    this.playerWin = "PLAYER1 WIN";
+    this.comPick;
+    this.playerPick;
+  }
+  // Com Pick
+  comOption() {
+    let comBrain = ["scissors", "rock", "paper"];
+    let comPick = comBrain[Math.floor(Math.random() * comBrain.length)];
+    return (this.comPick = comPick);
+  }
+
+  // Winner Calculation
+  winner(player, com) {
+    if (player == com) return "DRAW";
+    if (player == "scissors") return com == "rock" ? this.comWin : this.playerWin;
+    if (player == "rock") return com == "paper" ? this.comWin : this.playerWin;
+    if (player == "paper") return com == "scissors" ? this.comWin : this.playerWin;
+  }
+}
+
+export { GameStart };
